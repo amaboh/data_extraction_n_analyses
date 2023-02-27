7a0953e68dc749dce3a7cdea60cf5b25ce5d4082
amaboh <amabocanon@gmail.com>
2023-02-27 11:20:46 +0000

    project set up

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+from youtube2text import Youtube2Text
+
+youtubeLink = "https://www.youtube.com/watch?v=VyGpknh4rPM&ab_channel=Dr.VanitaRattan"
+
+converter = Youtube2Text()
+
+converter.url2text(youtubeLink)
